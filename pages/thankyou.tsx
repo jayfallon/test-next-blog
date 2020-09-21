64242d0c800c1c28f99ae0638d7d18e3bf54f56c
Jay Fallon <jayfallon@gmail.com>
2020-09-21 10:56:06 -0400

    added thankyou page

---
+++ pages/thankyou.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ThankYou() {
+  return (
+    <React.Fragment>
+      <h1>Thank you!</h1>
+      <p>Your submission has been received</p>
+    </React.Fragment>
+  );
+}
