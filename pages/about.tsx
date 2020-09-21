54089b7072ee21e584102a3a6b61b0b74e3c89f8
Jay Fallon <jayfallon@gmail.com>
2020-09-21 10:17:29 -0400

    removed about page

--- pages/about.tsx
+++
@@ -1,9 +0,0 @@
-import { Article } from "components/article";
-
-export default function About() {
-  return (
-    <div className="container">
-      <Article>this is an article</Article>
-    </div>
-  );
-}
